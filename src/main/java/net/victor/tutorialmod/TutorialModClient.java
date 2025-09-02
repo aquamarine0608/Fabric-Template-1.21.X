933c2475075931eef45728c268b5972824b1af72
aquamarine0608 <victorxu0608@gmail.com>
2025-09-02 16:18:13 -0400

    Initial commit

---
+++ src/main/java/net/victor/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.victor.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
